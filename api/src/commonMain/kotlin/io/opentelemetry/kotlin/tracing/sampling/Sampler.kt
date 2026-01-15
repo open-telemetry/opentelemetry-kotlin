3792f75cb5461b3dff592b76756ded8e2c82e379
Jamie Lynch <fractalwrench@gmail.com>
2026-01-15 16:38:36 +0000

    feat: supply Sampler API

---
+++ api/src/commonMain/kotlin/io/opentelemetry/kotlin/tracing/sampling/Sampler.kt
@@ -0,0 +1,41 @@
+package io.opentelemetry.kotlin.tracing.sampling
+
+import io.opentelemetry.kotlin.ExperimentalApi
+import io.opentelemetry.kotlin.attributes.AttributeContainer
+import io.opentelemetry.kotlin.context.Context
+import io.opentelemetry.kotlin.tracing.model.Link
+import io.opentelemetry.kotlin.tracing.model.SpanKind
+
+/**
+ * Decides whether a [io.opentelemetry.kotlin.tracing.model.Span] should be sampled or not
+ * by using information just before it's created to return a [SamplingResult].
+ *
+ * https://opentelemetry.io/docs/specs/otel/trace/sdk/#sampler
+ */
+@ExperimentalApi
+public interface Sampler {
+
+    /**
+     * Returns whether the span should be sampled or not.
+     *
+     * @param context A context containing the parent span
+     * @param traceId The traceId of the span to be created
+     * @param name The name of the span to be created
+     * @param spanKind The spanKind of the span to be created
+     * @param attributes The initial set of attributes of the span to be created
+     * @param links A collection of links that will be associated with the created span
+     */
+    public fun shouldSample(
+        context: Context,
+        traceId: String,
+        name: String,
+        spanKind: SpanKind,
+        attributes: AttributeContainer,
+        links: List<Link>,
+    ): SamplingResult
+
+    /**
+     * The name of the sampler or a short description that may be displayed in debug pages/logs.
+     */
+    public val description: String
+}
---
+++ api/src/commonMain/kotlin/io/opentelemetry/kotlin/tracing/sampling/SamplingResult.kt
@@ -0,0 +1,45 @@
+package io.opentelemetry.kotlin.tracing.sampling
+
+import io.opentelemetry.kotlin.ExperimentalApi
+import io.opentelemetry.kotlin.attributes.AttributeContainer
+import io.opentelemetry.kotlin.tracing.model.TraceState
+
+/**
+ * The result of a decision on whether a [io.opentelemetry.kotlin.tracing.model.Span] should
+ * be sampled or not.
+ */
+@ExperimentalApi
+public interface SamplingResult {
+
+    /**
+     * The sampling decision
+     */
+    public enum class Decision {
+
+        /**
+         * The span will not be recorded and it will not be sampled.
+         */
+        DROP,
+
+        /**
+         * The span will be recorded but it will not be sampled.
+         */
+        RECORD_ONLY,
+
+        /**
+         * The span will be recorded and it will be sampled.
+         */
+        RECORD_AND_SAMPLE,
+    }
+
+    /**
+     * The attributes that were added to the span.
+     */
+    public val attributes: AttributeContainer
+
+    /**
+     * The [TraceState] that will be associated with the [io.opentelemetry.kotlin.tracing.model.Span]
+     * through the new [io.opentelemetry.kotlin.tracing.model.SpanContext].
+     */
+    public val traceState: TraceState
+}
