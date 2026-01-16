60ff6a7ad56e6085df5dc21bffe3653371d5f87d
Jamie Lynch <fractalwrench@gmail.com>
2026-01-16 12:30:33 +0000

    feat: support AutoCloseable on span

--- api-ext/src/commonMain/kotlin/io/opentelemetry/kotlin/tracing/SpanExt.kt
+++ api-ext/src/commonMain/kotlin/io/opentelemetry/kotlin/tracing/SpanExt.kt
@@ -4,6 +4,7 @@ import io.opentelemetry.kotlin.ExperimentalApi
 import io.opentelemetry.kotlin.ThreadSafe
 import io.opentelemetry.kotlin.attributes.MutableAttributeContainer
 import io.opentelemetry.kotlin.exceptionType
+import io.opentelemetry.kotlin.tracing.data.StatusData
 import io.opentelemetry.kotlin.tracing.model.Span
 
 /**
@@ -30,8 +31,28 @@ public fun Span.recordException(
 /**
  * Adds a link to the span that associates it with another [Span].
  */
-@OptIn(ExperimentalApi::class)
+@ExperimentalApi
 @ThreadSafe
 public fun Span.addLink(span: Span, attributes: MutableAttributeContainer.() -> Unit = {}) {
     addLink(span.spanContext, attributes)
+}
+
+/**
+ * Wraps the [action] with a span that automatically ends when the [action] completes. This
+ * provides an alternative to manually ending spans. [action] must return the span
+ * status - generally this will be [StatusData.Ok] unless the operation fails.
+ *
+ * If an exception is thrown it will be added to the span as an event and the status will be
+ * set to [StatusData.Error] with a description of the throwable's message, if any.
+ */
+@ExperimentalApi
+public fun Span.wrapOperation(action: () -> StatusData) {
+    try {
+        status = action()
+    } catch (exc: Throwable) {
+        recordException(exc)
+        status = StatusData.Error(exc.message)
+    } finally {
+        end()
+    }
 }
