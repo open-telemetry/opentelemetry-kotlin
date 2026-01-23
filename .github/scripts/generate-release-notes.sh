33ce7a1f240d8a11b9c74c1c3060e6a0aa1ac0d4
Jamie Lynch <fractalwrench@gmail.com>
2026-01-23 10:59:47 +0000

    ci: setup release workflows

---
+++ .github/scripts/generate-release-notes.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+
+# Generates release notes, like what appears in GitHub release pages.
+
+VERSION=$1
+PRIOR_VERSION=$2
+
+sed -n "0,/^## Version $VERSION /d;/^## Version /q;p" CHANGELOG.md > /tmp/CHANGELOG_SECTION.md
+
+# the complex perl regex is needed because markdown docs render newlines as soft wraps
+# while release notes render them as line breaks
+perl -0pe 's/(?<!\n)\n *(?!\n)(?![-*] )(?![1-9]+\. )/ /g' /tmp/CHANGELOG_SECTION.md \
+    >> /tmp/release-notes.txt
+
+cat >> /tmp/release-notes.txt << EOF
+ ### 🙇 Thank you
+
+ This release was possible thanks to the following contributors:
+
+EOF
+
+.github/scripts/generate-release-contributors.sh "v${PRIOR_VERSION}" >> /tmp/release-notes.txt
