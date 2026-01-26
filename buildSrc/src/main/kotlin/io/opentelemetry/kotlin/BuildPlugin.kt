72411aed6bd7cc2bbb1b7f0950ed8b20e60b5589
Francisco Prieto <priettt@gmail.com>
2026-01-26 13:37:48 -0300

    Fix BuildPlugin structure and compat-bom version resolution

--- buildSrc/src/main/kotlin/io/opentelemetry/kotlin/BuildPlugin.kt
+++ buildSrc/src/main/kotlin/io/opentelemetry/kotlin/BuildPlugin.kt
@@ -10,24 +10,24 @@ class BuildPlugin : Plugin<Project> {
 
     override fun apply(project: Project) {
         project.pluginManager.withPlugin("org.jetbrains.kotlin.multiplatform") {
+            val kotlin = project.extensions.getByType(KotlinMultiplatformExtension::class.java)
+            project.configureKotlin(kotlin)
+            project.configureDetekt()
+            project.configureBinaryCompatValidation()
+            project.configureExplicitApiMode(kotlin)
+            project.configureTest()
+
             project.pluginManager.withPlugin("com.android.kotlin.multiplatform.library") {
-                val kotlin = project.extensions.getByType(KotlinMultiplatformExtension::class.java)
-                // Configure the android target - it's registered as an extension on the kotlin extension
                 val androidTarget = kotlin.extensions.getByType(KotlinMultiplatformAndroidLibraryTarget::class.java)
                 project.configureKotlinAndroidTarget(androidTarget)
-                project.configureKotlin(kotlin)
-                project.configureDetekt()
-                project.configureBinaryCompatValidation()
-                project.configureExplicitApiMode(kotlin)
-                project.configureTest()
             }
-            project.pluginManager.withPlugin("org.jetbrains.kotlin.jvm") {
-                val kotlin = project.extensions.getByType(KotlinJvmProjectExtension::class.java)
-                project.configureDetekt()
-                project.configureBinaryCompatValidation()
-                project.configureTest()
-            }
-            project.configurePublishing()
         }
+        project.pluginManager.withPlugin("org.jetbrains.kotlin.jvm") {
+            val kotlin = project.extensions.getByType(KotlinJvmProjectExtension::class.java)
+            project.configureDetekt()
+            project.configureBinaryCompatValidation()
+            project.configureTest()
+        }
+        project.configurePublishing()
     }
 }
