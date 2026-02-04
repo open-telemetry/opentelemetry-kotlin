5d71f0c1e442f06ba70318fadd1f3a8dbc520bbc
Jamie Lynch <fractalwrench@gmail.com>
2026-02-04 07:49:12 +0000

    ci: alter scripts to error earlier

--- .github/scripts/get-version.sh
+++ .github/scripts/get-version.sh
@@ -1,3 +1,8 @@
 #!/bin/bash -e
 
-grep ^version= gradle.properties | sed s/version=// | tr -d '\r'
+[[ ! -f "gradle.properties" ]] && { echo "Error: gradle.properties not found" >&2; exit 1; }
+
+version=$(grep ^version= gradle.properties | sed s/version=// | tr -d '\r')
+[[ -z "$version" ]] && { echo "Error: version not found in gradle.properties" >&2; exit 1; }
+
+echo "$version"
