22d6c9d1a6e19dea7c690ec63629937bd06eab30
bidetofevil <hanson.ho@embrace.io>
2026-02-23 14:29:47 -0800

    Move helper methods that act on ShutdownState to sdk-common

--- sdk-api/src/commonMain/kotlin/io/opentelemetry/kotlin/export/ShutdownState.kt
+++ sdk-api/src/commonMain/kotlin/io/opentelemetry/kotlin/export/ShutdownState.kt
@@ -3,7 +3,7 @@ package io.opentelemetry.kotlin.export
 import io.opentelemetry.kotlin.ExperimentalApi
 
 /**
- * Execute code or return defaults depending on whether this is shutdown or not, respectively.
+ * Read-only view of whether a component is shutdown or not.
  */
 @ExperimentalApi
 public abstract class ShutdownState {
@@ -12,33 +12,4 @@ public abstract class ShutdownState {
      * Whether this is shutdown or not
      */
     public abstract val isShutdown: Boolean
-
-    /**
-     * Run the given [action] if this isn't shutdown. Otherwise, return [default].
-     * This method will not handle exceptions thrown by [action].
-     */
-    public inline fun <T> ifActiveOrElse(default: T, action: () -> T): T =
-        if (isShutdown) {
-            default
-        } else {
-            action()
-        }
-
-    /**
-     * Run the given [action] and return the resulting [OperationResultCode] if this isn't shutdown.
-     * Otherwise, return [OperationResultCode.Failure].
-     * This method will not handle exceptions thrown by [action].
-     */
-    public inline fun ifActive(
-        action: () -> OperationResultCode,
-    ): OperationResultCode =
-        ifActiveOrElse(OperationResultCode.Failure, action)
-
-    /**
-     * Run the given [action] if this isn't shutdown. Do nothing otherwise.
-     * This method will not handle exceptions thrown by [action].
-     */
-    public inline fun execute(action: () -> Unit) {
-        ifActiveOrElse(Unit, action)
-    }
 }
