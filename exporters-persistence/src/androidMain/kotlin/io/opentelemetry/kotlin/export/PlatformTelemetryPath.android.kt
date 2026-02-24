7c9ea45962d522473c6463e898de678defa9f32b
Jamie Lynch <fractalwrench@gmail.com>
2026-02-24 09:12:42 +0000

    refactor: alter how the storage directory is defined (#216)

--- exporters-persistence/src/androidMain/kotlin/io/opentelemetry/kotlin/export/PlatformTelemetryPath.android.kt
+++ exporters-persistence/src/androidMain/kotlin/io/opentelemetry/kotlin/export/PlatformTelemetryPath.android.kt
@@ -2,10 +2,6 @@ package io.opentelemetry.kotlin.export
 
 import io.opentelemetry.kotlin.ExperimentalApi
 import okio.FileSystem
-import okio.Path
-
-@ExperimentalApi
-internal actual fun getCacheDirectory(): Path = throw UnsupportedOperationException()
 
 @ExperimentalApi
 internal actual fun getFileSystem(): FileSystem = FileSystem.SYSTEM
--- exporters-persistence/src/commonMain/kotlin/io/opentelemetry/kotlin/export/PlatformTelemetryPath.kt
+++ exporters-persistence/src/commonMain/kotlin/io/opentelemetry/kotlin/export/PlatformTelemetryPath.kt
@@ -5,12 +5,6 @@ import okio.FileSystem
 import okio.Path
 
 /**
- * Returns the base directory that should be used to store cached data.
- */
-@ExperimentalApi
-internal expect fun getCacheDirectory(): Path
-
-/**
  * Returns an Okio representation of the file system.
  */
 @ExperimentalApi
@@ -20,6 +14,9 @@ internal expect fun getFileSystem(): FileSystem
  * Returns the directory used to store telemetry for the given [PersistedTelemetryType].
  */
 @ExperimentalApi
-internal fun getTelemetryStorageDirectory(type: PersistedTelemetryType): Path {
-    return getCacheDirectory() / "/opentelemetry-kotlin/persisted-telemetry" / type.directoryName
+internal fun getTelemetryStorageDirectory(
+    cacheDirectory: Path,
+    type: PersistedTelemetryType
+): Path {
+    return cacheDirectory / "/opentelemetry-kotlin/persisted-telemetry" / type.directoryName
 }
