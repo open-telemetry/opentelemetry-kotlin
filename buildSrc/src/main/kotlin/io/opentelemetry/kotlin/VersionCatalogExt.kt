c3b0b4d201f4c4798b5e43489f073a3051989153
Jamie Lynch <fractalwrench@gmail.com>
2026-02-24 09:26:24 +0000

    build: define min AGP version

--- buildSrc/src/main/kotlin/io/opentelemetry/kotlin/VersionCatalogExt.kt
+++ buildSrc/src/main/kotlin/io/opentelemetry/kotlin/VersionCatalogExt.kt
@@ -7,3 +7,6 @@ import org.gradle.kotlin.dsl.getByType
 // workaround: see https://medium.com/@saulmm2/android-gradle-precompiled-scripts-tomls-kotlin-dsl-df3c27ea017c
 fun Project.findLibrary(alias: String) =
     project.extensions.getByType<VersionCatalogsExtension>().named("libs").findLibrary(alias).get()
+
+fun Project.findVersion(alias: String) =
+    project.extensions.getByType<VersionCatalogsExtension>().named("libs").findVersion(alias).get().toString()
