6961c8735ed7bf3828f0ca7dafb352d07d4e32fb
Jamie Lynch <fractalwrench@gmail.com>
2026-03-30 13:47:37 +0100

    fix build

--- compat/src/jvmAndAndroidMain/kotlin/io/opentelemetry/kotlin/tracing/model/OtelJavaSpanAdapter.kt
+++ compat/src/jvmAndAndroidMain/kotlin/io/opentelemetry/kotlin/tracing/model/OtelJavaSpanAdapter.kt
@@ -9,8 +9,8 @@ import io.opentelemetry.kotlin.aliases.OtelJavaSpan
 import io.opentelemetry.kotlin.aliases.OtelJavaSpanContext
 import io.opentelemetry.kotlin.aliases.OtelJavaStatusCode
 import io.opentelemetry.kotlin.attributes.convertToMap
-import io.opentelemetry.kotlin.tracing.Span
 import io.opentelemetry.kotlin.attributes.setExceptionAttributes
+import io.opentelemetry.kotlin.tracing.Span
 import io.opentelemetry.kotlin.tracing.ext.toOtelJavaSpanContext
 import io.opentelemetry.kotlin.tracing.ext.toOtelKotlinStatusData
 import java.util.concurrent.TimeUnit
