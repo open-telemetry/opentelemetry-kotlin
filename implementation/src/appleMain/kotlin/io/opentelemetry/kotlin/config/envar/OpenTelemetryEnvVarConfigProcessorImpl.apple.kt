0c5044cfc3e9103794dc3a955f339158788b1508
Viorel Alexandrescu <01-corded.syllabi@icloud.com>
2026-04-21 21:49:28 +0300

    removing apple and js implementations as we have general implementations covering those targets (native and web module)

--- implementation/src/appleMain/kotlin/io/opentelemetry/kotlin/config/envar/OpenTelemetryEnvVarConfigProcessorImpl.apple.kt
+++
@@ -1,6 +0,0 @@
-package io.opentelemetry.kotlin.config.envar
-
-/**
- * No-Op Implementation as target should not support envar configuration
- */
-internal actual fun getEnvVarValue(envVar: String): String? = null
