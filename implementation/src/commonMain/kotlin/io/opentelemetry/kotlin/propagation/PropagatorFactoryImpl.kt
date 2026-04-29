dc714dee4ab346d608be5e39f53a0cd8e757eb02
Jamie Lynch <fractalwrench@gmail.com>
2026-04-29 15:48:36 +0100

    begin implementing propagator api (#455)

---
+++ implementation/src/commonMain/kotlin/io/opentelemetry/kotlin/propagation/PropagatorFactoryImpl.kt
@@ -0,0 +1,26 @@
+package io.opentelemetry.kotlin.propagation
+
+import io.opentelemetry.kotlin.ExperimentalApi
+import io.opentelemetry.kotlin.context.Context
+
+@OptIn(ExperimentalApi::class)
+internal class PropagatorFactoryImpl : PropagatorFactory {
+
+    override fun composite(vararg propagators: TextMapPropagator): TextMapPropagator {
+        return composite(propagators.toList())
+    }
+
+    override fun composite(propagators: List<TextMapPropagator>): TextMapPropagator {
+        return when (propagators.size) {
+            0 -> EmptyTextMapPropagator
+            1 -> propagators.single()
+            else -> CompositeTextMapPropagator(propagators.toList())
+        }
+    }
+
+    private object EmptyTextMapPropagator : TextMapPropagator {
+        override fun fields(): Collection<String> = emptyList()
+        override fun <T> inject(context: Context, carrier: T, setter: TextMapSetter<T>) {}
+        override fun <T> extract(context: Context, carrier: T, getter: TextMapGetter<T>): Context = context
+    }
+}
---
+++ implementation/src/commonTest/kotlin/io/opentelemetry/kotlin/propagation/MapCarrierFixtures.kt
@@ -0,0 +1,16 @@
+package io.opentelemetry.kotlin.propagation
+
+import io.opentelemetry.kotlin.ExperimentalApi
+
+@OptIn(ExperimentalApi::class)
+internal object MapTextMapGetter : TextMapGetter<Map<String, String>> {
+    override fun keys(carrier: Map<String, String>): Collection<String> = carrier.keys
+    override fun get(carrier: Map<String, String>, key: String): String? = carrier[key]
+}
+
+@OptIn(ExperimentalApi::class)
+internal object MapTextMapSetter : TextMapSetter<MutableMap<String, String>> {
+    override fun set(carrier: MutableMap<String, String>, key: String, value: String) {
+        carrier[key] = value
+    }
+}
