1092eeacbeeac473d6420e0ee0bbc1d63701a65f
Jamie Lynch <fractalwrench@gmail.com>
2026-05-05 09:38:03 +0100

    allow supplying a custom implicit context storage mechanism (#470)

--- sdk-api/src/commonMain/kotlin/io/opentelemetry/kotlin/init/ContextConfigDsl.kt
+++ sdk-api/src/commonMain/kotlin/io/opentelemetry/kotlin/init/ContextConfigDsl.kt
@@ -1,6 +1,7 @@
 package io.opentelemetry.kotlin.init
 
 import io.opentelemetry.kotlin.ExperimentalApi
+import io.opentelemetry.kotlin.context.ImplicitContextStorage
 import io.opentelemetry.kotlin.context.ImplicitContextStorageMode
 
 /**
@@ -11,7 +12,14 @@ import io.opentelemetry.kotlin.context.ImplicitContextStorageMode
 public interface ContextConfigDsl {
 
     /**
-     * Defines the storage mechanism used for the implicit context.
+     * Selects among the built-in [ImplicitContextStorage] implementations. Ignored when a custom
+     * implementation is supplied via [storage].
      */
     public var storageMode: ImplicitContextStorageMode
+
+    /**
+     * Plugs in a custom [ImplicitContextStorage] implementation. When set, this takes precedence
+     * over [storageMode]. May only be called once.
+     */
+    public fun storage(action: () -> ImplicitContextStorage)
 }
