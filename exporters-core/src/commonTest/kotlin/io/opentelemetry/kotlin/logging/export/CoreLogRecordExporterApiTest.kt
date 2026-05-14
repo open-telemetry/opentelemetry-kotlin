f18961df9e84f246ee0fcf7e48cfc057e0a615a1
bidetofevil <hanson.ho@embrace.io>
2026-05-14 16:05:39 -0700

    Add empty composite exporter tests

--- exporters-core/src/commonTest/kotlin/io/opentelemetry/kotlin/logging/export/CoreLogRecordExporterApiTest.kt
+++ exporters-core/src/commonTest/kotlin/io/opentelemetry/kotlin/logging/export/CoreLogRecordExporterApiTest.kt
@@ -1,20 +1,42 @@
 package io.opentelemetry.kotlin.logging.export
 
+import io.opentelemetry.kotlin.FakeInstrumentationScopeInfo
+import io.opentelemetry.kotlin.context.FakeContext
 import io.opentelemetry.kotlin.export.FakeLogExportConfig
+import io.opentelemetry.kotlin.export.OperationResultCode
+import io.opentelemetry.kotlin.logging.model.FakeReadWriteLogRecord
+import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
 import kotlin.test.assertSame
 
 internal class CoreLogRecordExporterApiTest {
 
     private val config = FakeLogExportConfig()
+    private val fakeLogRecord = FakeReadWriteLogRecord()
+    private val fakeContext = FakeContext()
+    private val scopeInfo = FakeInstrumentationScopeInfo()
 
     @Test
-    fun compositeLogRecordProcessorEmptyReturnsNoop() {
-        assertSame(NoopLogRecordProcessor, config.compositeLogRecordProcessor())
+    fun compositeLogRecordProcessorEmptyReturnsNoopWithoutThrowing() = runTest {
+        val emptyProcessor = config.compositeLogRecordProcessor().apply {
+            onEmit(fakeLogRecord, fakeContext)
+            assertFalse(enabled(fakeContext, scopeInfo, null, null))
+            assertEquals(OperationResultCode.Success, forceFlush())
+            assertEquals(OperationResultCode.Success, shutdown())
+        }
+
+        assertSame(NoopLogRecordProcessor, emptyProcessor)
     }
 
     @Test
-    fun compositeLogRecordExporterEmptyReturnsNoop() {
-        assertSame(NoopLogRecordExporter, config.compositeLogRecordExporter())
+    fun compositeLogRecordExporterEmptyReturnsNoopWithoutThrowing() = runTest {
+        val emptyExporter = config.compositeLogRecordExporter().apply {
+            assertEquals(OperationResultCode.Failure, export(listOf(fakeLogRecord)))
+            assertEquals(OperationResultCode.Success, forceFlush())
+            assertEquals(OperationResultCode.Success, shutdown())
+        }
+        assertSame(NoopLogRecordExporter, emptyExporter)
     }
 }
--- exporters-core/src/commonTest/kotlin/io/opentelemetry/kotlin/tracing/export/CoreSpanExporterApiTest.kt
+++ exporters-core/src/commonTest/kotlin/io/opentelemetry/kotlin/tracing/export/CoreSpanExporterApiTest.kt
@@ -1,20 +1,40 @@
 package io.opentelemetry.kotlin.tracing.export
 
+import io.opentelemetry.kotlin.context.FakeContext
 import io.opentelemetry.kotlin.export.FakeTraceExportConfig
+import io.opentelemetry.kotlin.export.OperationResultCode
+import io.opentelemetry.kotlin.tracing.FakeReadWriteSpan
+import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertSame
 
 internal class CoreSpanExporterApiTest {
 
     private val config = FakeTraceExportConfig()
+    private val fakeSpan = FakeReadWriteSpan()
+    private val fakeContext = FakeContext()
 
     @Test
-    fun compositeSpanProcessorEmptyReturnsNoop() {
-        assertSame(NoopSpanProcessor, config.compositeSpanProcessor())
+    fun compositeSpanProcessorEmptyReturnsNoopWithoutThrowing() = runTest {
+        val emptyProcessor = config.compositeSpanProcessor().apply {
+            onStart(fakeSpan, fakeContext)
+            onEnding(fakeSpan)
+            onEnd(fakeSpan)
+            assertEquals(OperationResultCode.Success, forceFlush())
+            assertEquals(OperationResultCode.Success, shutdown())
+        }
+
+        assertSame(NoopSpanProcessor, emptyProcessor)
     }
 
     @Test
-    fun compositeSpanExporterEmptyReturnsNoop() {
-        assertSame(NoopSpanExporter, config.compositeSpanExporter())
+    fun compositeSpanExporterEmptyReturnsNoopWithoutThrowing() = runTest {
+        val emptyExporter = config.compositeSpanExporter().apply {
+            assertEquals(OperationResultCode.Failure, export(listOf(fakeSpan)))
+            assertEquals(OperationResultCode.Success, forceFlush())
+            assertEquals(OperationResultCode.Success, shutdown())
+        }
+        assertSame(NoopSpanExporter, emptyExporter)
     }
 }
