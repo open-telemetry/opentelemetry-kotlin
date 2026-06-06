8d7d2724131011ba604651b5a9cc932d3c674abe
Narumichi Kubo <narumichi7373@gmail.com>
2026-06-06 13:57:39 +0700

    fix: Use TraceService response model for OTLP trace response

--- exporters-protobuf/src/commonMain/kotlin/io/opentelemetry/kotlin/tracing/export/ExportTracesResponseDeserializer.kt
+++ exporters-protobuf/src/commonMain/kotlin/io/opentelemetry/kotlin/tracing/export/ExportTracesResponseDeserializer.kt
@@ -1,6 +1,6 @@
 package io.opentelemetry.kotlin.tracing.export
 
-import io.opentelemetry.proto.collector.logs.v1.ExportLogsServiceResponse
+import io.opentelemetry.proto.collector.trace.v1.ExportTraceServiceResponse
 
 fun ByteArray.deserializeTraceRecordErrorMessage() =
-    ExportLogsServiceResponse.ADAPTER.decode(this).partial_success?.error_message
+    ExportTraceServiceResponse.ADAPTER.decode(this).partial_success?.error_message
