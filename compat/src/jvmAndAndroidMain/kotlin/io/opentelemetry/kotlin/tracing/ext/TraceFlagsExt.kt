9595ca8ec47019bdee20f1a36f920a295b2f5826
Msksgm <msksgm3124@gmail.com>
2026-06-11 07:54:24 +0900

    fix: the bug of trace flags' hex algorythm from flags

--- compat/src/jvmAndAndroidMain/kotlin/io/opentelemetry/kotlin/tracing/ext/TraceFlagsExt.kt
+++ compat/src/jvmAndAndroidMain/kotlin/io/opentelemetry/kotlin/tracing/ext/TraceFlagsExt.kt
@@ -4,18 +4,11 @@ import io.opentelemetry.kotlin.aliases.OtelJavaTraceFlags
 import io.opentelemetry.kotlin.tracing.TraceFlags
 
 internal fun TraceFlags.toOtelJavaTraceFlags(): OtelJavaTraceFlags {
-    val sb = StringBuilder()
-    sb.append(
-        when {
-            isRandom -> "1"
-            else -> "0"
-        }
-    )
-    sb.append(
-        when {
-            isSampled -> "1"
-            else -> "0"
-        }
-    )
-    return OtelJavaTraceFlags.fromHex(sb.toString(), 0)
+    val traceFlagsHex = when {
+        isRandom && isSampled -> "03"
+        isRandom && !isSampled -> "02"
+        !isRandom && isSampled -> "01"
+        else -> "00"
+    }
+    return OtelJavaTraceFlags.fromHex(traceFlagsHex, 0)
 }
