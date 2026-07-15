1ae3b9ba7ee0a8fe76ad5fb4a1b419d76291ef59
Jamie Lynch <fractalwrench@gmail.com>
2026-07-15 09:35:24 +0100

    build: download opentelemetry-configuration schema (#661)

--- config/build.gradle.kts
+++ config/build.gradle.kts
@@ -1,3 +1,5 @@
+import de.undercouch.gradle.tasks.download.Download
+
 plugins {
     kotlin("multiplatform")
     id("com.android.kotlin.multiplatform.library")
@@ -5,7 +7,13 @@ plugins {
     id("signing")
     id("com.vanniktech.maven.publish")
     id("org.jetbrains.kotlinx.kover")
+    alias(libs.plugins.download)
 }
+
+// release version of https://github.com/open-telemetry/opentelemetry-configuration
+val openTelemetryConfigurationVersion = "1.1.0"
+val openTelemetryConfigurationRepoZip =
+    "https://github.com/open-telemetry/opentelemetry-configuration/archive/v${openTelemetryConfigurationVersion}.zip"
 
 kotlin {
     sourceSets {
@@ -14,5 +22,41 @@ kotlin {
                 implementation(project(":sdk-api"))
             }
         }
+    }
+}
+
+val downloadOpenTelemetryConfiguration by tasks.registering(Download::class) {
+    src(openTelemetryConfigurationRepoZip)
+    dest(
+        layout.buildDirectory.file(
+            "opentelemetry-configuration-${openTelemetryConfigurationVersion}/opentelemetry-configuration.zip"
+        )
+    )
+    overwrite(false)
+}
+
+val refreshOpenTelemetryConfiguration by tasks.registering(Copy::class) {
+    dependsOn(downloadOpenTelemetryConfiguration)
+
+    from(zipTree(downloadOpenTelemetryConfiguration.get().dest))
+    eachFile(closureOf<FileCopyDetails> {
+        // remove top level folder (opentelemetry-configuration-<version>/)
+        val pathParts = path.split("/")
+        path = pathParts.subList(1, pathParts.size).joinToString("/")
+    })
+    into(layout.buildDirectory.dir("opentelemetry-configuration-${openTelemetryConfigurationVersion}/"))
+}
+
+// This task will generate Kotlin data-model classes from the downloaded opentelemetry-configuration
+// JSON schema. It's currently a no-op.
+tasks.register("generateOpenTelemetryConfiguration") {
+    dependsOn(refreshOpenTelemetryConfiguration)
+    val schemaDir =
+        layout.buildDirectory.dir("opentelemetry-configuration-${openTelemetryConfigurationVersion}")
+    doLast {
+        logger.lifecycle(
+            "generateOpenTelemetryConfiguration is a no-op placeholder; " +
+                    "schema available at ${schemaDir.get().asFile}"
+        )
     }
 }
