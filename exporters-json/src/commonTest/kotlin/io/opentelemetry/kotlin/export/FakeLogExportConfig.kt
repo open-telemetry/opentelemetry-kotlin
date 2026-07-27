67803da3513fc054fd2d1c29e60c354991fb16ad
Viorel Alexandrescu <01-corded.syllabi@icloud.com>
2026-07-27 17:35:17 +0300

    fix fake log export config classes

--- exporters-json/src/commonTest/kotlin/io/opentelemetry/kotlin/export/FakeLogExportConfig.kt
+++ exporters-json/src/commonTest/kotlin/io/opentelemetry/kotlin/export/FakeLogExportConfig.kt
@@ -2,8 +2,11 @@ package io.opentelemetry.kotlin.export
 
 import io.opentelemetry.kotlin.Clock
 import io.opentelemetry.kotlin.clock.FakeClock
+import io.opentelemetry.kotlin.error.NoopSdkErrorHandler
+import io.opentelemetry.kotlin.error.SdkErrorHandler
 import io.opentelemetry.kotlin.init.LogExportConfigDsl
 
 internal class FakeLogExportConfig(
-    override val clock: Clock = FakeClock()
+    override val clock: Clock = FakeClock(),
+    override val sdkErrorHandler: SdkErrorHandler = NoopSdkErrorHandler,
 ) : LogExportConfigDsl
