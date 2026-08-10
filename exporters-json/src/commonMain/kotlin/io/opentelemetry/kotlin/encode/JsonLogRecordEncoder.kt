2cdf1c3dda08dddc0878ad9e18f64553c6faf2dc
Viorel Alexandrescu <01-corded.syllabi@icloud.com>
2026-08-10 10:51:41 +0300

    revert encoder hierarchy implementation

--- exporters-json/src/commonMain/kotlin/io/opentelemetry/kotlin/encode/JsonLogRecordEncoder.kt
+++ exporters-json/src/commonMain/kotlin/io/opentelemetry/kotlin/encode/JsonLogRecordEncoder.kt
@@ -1,14 +1,12 @@
 package io.opentelemetry.kotlin.encode
 
-import io.opentelemetry.kotlin.framework.serialization.SerializableLogRecordData
 import io.opentelemetry.kotlin.framework.serialization.conversion.toSerializable
 import io.opentelemetry.kotlin.logging.model.ReadableLogRecord
-import kotlinx.serialization.KSerializer
+import kotlinx.serialization.json.Json
 
-internal class JsonLogRecordEncoder : OtlpJsonEncoder<ReadableLogRecord, SerializableLogRecordData> {
-    override fun getSerializable(value: ReadableLogRecord): SerializableLogRecordData =
-        value.toSerializable()
-
-    override fun getSerializer(): KSerializer<SerializableLogRecordData> =
-        SerializableLogRecordData.serializer()
+internal class JsonLogRecordEncoder : OtlpJsonEncoder<ReadableLogRecord> {
+    override fun encode(value: ReadableLogRecord): Sequence<String> =
+        sequence {
+            yield(Json.encodeToString(value.toSerializable()))
+        }
 }
--- exporters-json/src/commonMain/kotlin/io/opentelemetry/kotlin/encode/JsonSpanEncoder.kt
+++ exporters-json/src/commonMain/kotlin/io/opentelemetry/kotlin/encode/JsonSpanEncoder.kt
@@ -1,14 +1,12 @@
 package io.opentelemetry.kotlin.encode
 
-import io.opentelemetry.kotlin.framework.serialization.SerializableSpanData
 import io.opentelemetry.kotlin.framework.serialization.conversion.toSerializable
 import io.opentelemetry.kotlin.tracing.data.SpanData
-import kotlinx.serialization.KSerializer
+import kotlinx.serialization.json.Json
 
-internal class JsonSpanEncoder : OtlpJsonEncoder<SpanData, SerializableSpanData> {
-    override fun getSerializable(value: SpanData): SerializableSpanData =
-        value.toSerializable()
-
-    override fun getSerializer(): KSerializer<SerializableSpanData> =
-        SerializableSpanData.serializer()
+internal class JsonSpanEncoder : OtlpJsonEncoder<SpanData> {
+    override fun encode(value: SpanData): Sequence<String> =
+        sequence {
+            yield(Json.encodeToString(value.toSerializable()))
+        }
 }
