71fc790fe691566cdbf9750242f1228e7edf2da0
shaurya2k06 <shaurya2k06@gmail.com>
2026-08-19 01:14:20 +0530

    fix: use spec-length zero IDs in NoopSpanContext
    Signed-off-by: shaurya2k06 <shaurya2k06@gmail.com>

--- noop/src/commonMain/kotlin/io/opentelemetry/kotlin/factory/NoopIdGenerator.kt
+++ noop/src/commonMain/kotlin/io/opentelemetry/kotlin/factory/NoopIdGenerator.kt
@@ -1,9 +1,8 @@
 package io.opentelemetry.kotlin.factory
 
 internal object NoopIdGenerator : IdGenerator {
-    private val empty = ByteArray(0)
-    override fun generateSpanIdBytes(): ByteArray = empty
-    override fun generateTraceIdBytes(): ByteArray = empty
-    override val invalidTraceId: ByteArray = empty
-    override val invalidSpanId: ByteArray = empty
+    override val invalidTraceId: ByteArray = ByteArray(16)
+    override val invalidSpanId: ByteArray = ByteArray(8)
+    override fun generateSpanIdBytes(): ByteArray = invalidSpanId
+    override fun generateTraceIdBytes(): ByteArray = invalidTraceId
 }
