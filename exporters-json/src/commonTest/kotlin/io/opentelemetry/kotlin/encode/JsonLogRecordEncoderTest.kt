84df1ba753e798f50a946ee32709d1448827bb35
Viorel Alexandrescu <01-corded.syllabi@icloud.com>
2026-08-19 15:56:22 +0300

    added unit tests checking actual value encoding for logrecords and spandata

--- exporters-json/src/commonTest/kotlin/io/opentelemetry/kotlin/encode/JsonLogRecordEncoderTest.kt
+++ exporters-json/src/commonTest/kotlin/io/opentelemetry/kotlin/encode/JsonLogRecordEncoderTest.kt
@@ -1,7 +1,11 @@
 package io.opentelemetry.kotlin.encode
 
+import io.opentelemetry.kotlin.framework.serialization.SerializableLogRecordData
+import io.opentelemetry.kotlin.framework.serialization.conversion.toSerializable
 import io.opentelemetry.kotlin.logging.model.FakeReadableLogRecord
+import kotlinx.serialization.json.Json
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 internal class JsonLogRecordEncoderTest {
@@ -15,6 +19,64 @@ internal class JsonLogRecordEncoderTest {
         val result = encoder.encode(FakeReadableLogRecord())
 
         // then
-        assertTrue { result.any() }
+        assertTrue {
+            result.any {
+                it == Json.encodeToString(
+                    Json.parseToJsonElement(logRecordJson)
+                )
+            }
+        }
     }
+
+    @Test
+    fun `should successfully decode a log record data in JSON format`() {
+        // given
+        val encoder = JsonLogRecordEncoder()
+        val value = FakeReadableLogRecord()
+
+        // when
+        val result = encoder.encode(value)
+
+        // then
+        assertEquals(
+            expected = value.toSerializable(),
+            actual = Json.decodeFromString<SerializableLogRecordData>(result.toList().first())
+        )
+    }
+
+    private val logRecordJson = """
+    {
+      "resource": {
+        "schemaUrl": "schemaUrl",
+        "attributes": {
+          "foo": "bar"
+        }
+      },
+      "instrumentationScopeInfo": {
+        "name": "name",
+        "version": "version",
+        "schemaUrl": "schemaUrl",
+        "attributes": {
+          "key": "value"
+        }
+      },
+      "timestampEpochNanos": 1000,
+      "observedTimestampEpochNanos": 2000,
+      "spanContext": {
+        "traceId": "00000000000000000000000000000000",
+        "spanId": "0000000000000000",
+        "traceFlags": "01",
+        "traceState": {
+          "foo": "bar"
+        }
+      },
+      "severity": "WARN",
+      "severityText": "warning",
+      "body": "Hello, World!",
+      "attributes": {
+        "key": "value"
+      },
+      "totalAttributeCount": 1
+    }
+""".trimIndent()
 }
--- exporters-json/src/commonTest/kotlin/io/opentelemetry/kotlin/encode/JsonSpanEncoderTest.kt
+++ exporters-json/src/commonTest/kotlin/io/opentelemetry/kotlin/encode/JsonSpanEncoderTest.kt
@@ -1,7 +1,11 @@
 package io.opentelemetry.kotlin.encode
 
+import io.opentelemetry.kotlin.framework.serialization.SerializableSpanData
+import io.opentelemetry.kotlin.framework.serialization.conversion.toSerializable
 import io.opentelemetry.kotlin.tracing.data.FakeSpanData
+import kotlinx.serialization.json.Json
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 internal class JsonSpanEncoderTest {
@@ -15,6 +19,104 @@ internal class JsonSpanEncoderTest {
         val result = encoder.encode(FakeSpanData())
 
         // then
-        assertTrue { result.any() }
+        assertTrue {
+            result.any {
+                it == Json.encodeToString(
+                    Json.parseToJsonElement(spanDataJson)
+                )
+            }
+        }
     }
+
+    @Test
+    fun `should successfully decode a log record data in JSON format`() {
+        // given
+        val encoder = JsonSpanEncoder()
+        val value = FakeSpanData()
+
+        // when
+        val result = encoder.encode(value)
+
+        // then
+        assertEquals(
+            expected = value.toSerializable(),
+            actual = Json.decodeFromString<SerializableSpanData>(result.toList().first())
+        )
+    }
+
+    private val spanDataJson = """
+    {
+      "name": "span",
+      "kind": "INTERNAL",
+      "statusData": {
+        "name": "OK",
+        "description": ""
+      },
+      "spanContext": {
+        "traceId": "00000000000000000000000000000000",
+        "spanId": "0000000000000000",
+        "traceFlags": "01",
+        "traceState": {
+          "foo": "bar"
+        }
+      },
+      "parentSpanContext": {
+        "traceId": "00000000000000000000000000000000",
+        "spanId": "0000000000000000",
+        "traceFlags": "01",
+        "traceState": {
+          "foo": "bar"
+        }
+      },
+      "startTimestamp": 1000,
+      "attributes": {
+        "key": "value"
+      },
+      "events": [
+        {
+          "name": "event",
+          "attributes": {
+            "key": "value"
+          },
+          "timestamp": 1000,
+          "totalAttributesCount": 1
+        }
+      ],
+      "links": [
+        {
+          "spanContext": {
+            "traceId": "00000000000000000000000000000000",
+            "spanId": "0000000000000000",
+            "traceFlags": "01",
+            "traceState": {
+              "foo": "bar"
+            }
+          },
+          "attributes": {
+            "key": "value"
+          },
+          "totalAttributeCount": 1
+        }
+      ],
+      "endTimestamp": 2000,
+      "ended": true,
+      "totalRecordedEvents": 1,
+      "totalRecordedLinks": 1,
+      "totalAttributeCount": 1,
+      "resource": {
+        "schemaUrl": "schemaUrl",
+        "attributes": {
+          "foo": "bar"
+        }
+      },
+      "instrumentationScopeInfo": {
+        "name": "name",
+        "version": "version",
+        "schemaUrl": "schemaUrl",
+        "attributes": {
+          "key": "value"
+        }
+      }
+    }
+""".trimIndent()
 }
