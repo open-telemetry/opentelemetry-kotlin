e28f098408bf570ada84601ef2be27ae992ec7cf
Jamie Lynch <fractalwrench@gmail.com>
2026-09-17 09:23:19 +0100

    address review feedback

--- compat/src/jvmAndAndroidMain/kotlin/io/opentelemetry/kotlin/baggage/BaggageExt.kt
+++ compat/src/jvmAndAndroidMain/kotlin/io/opentelemetry/kotlin/baggage/BaggageExt.kt
@@ -5,12 +5,13 @@ import io.opentelemetry.kotlin.aliases.OtelJavaBaggage
 import io.opentelemetry.kotlin.aliases.OtelJavaBaggageEntryMetadata
 
 @OptIn(ExperimentalApi::class)
-public fun Baggage.toOtelJavaBaggage(): OtelJavaBaggage {
-    (this as? BaggageAdapter)?.let { return it.impl }
-
-    val builder = OtelJavaBaggage.builder()
-    asMap().forEach { (name, entry) ->
-        builder.put(name, entry.value, OtelJavaBaggageEntryMetadata.create(entry.metadata.value))
+public fun Baggage.toOtelJavaBaggage(): OtelJavaBaggage = when (this) {
+    is BaggageAdapter -> impl
+    else -> {
+        val builder = OtelJavaBaggage.builder()
+        asMap().forEach { (name, entry) ->
+            builder.put(name, entry.value, OtelJavaBaggageEntryMetadata.create(entry.metadata.value))
+        }
+        builder.build()
     }
-    return builder.build()
 }
