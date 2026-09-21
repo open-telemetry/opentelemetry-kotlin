c873859306858bf8d7c143f113ffa5252624e9b2
Aarav Mahajan <aaravmahajan2003@gmail.com>
2026-09-21 22:26:26 +0530

    docs: clarify KDoc
    Signed-off-by: Aarav Mahajan <aaravmahajan2003@gmail.com>

--- platform-implementations/src/androidMain/kotlin/io/opentelemetry/kotlin/ClockImpl.android.kt
+++ platform-implementations/src/androidMain/kotlin/io/opentelemetry/kotlin/ClockImpl.android.kt
@@ -7,9 +7,9 @@ import android.os.SystemClock
  * [System.currentTimeMillis] and then adds that baseline to
  * [SystemClock.elapsedRealtimeNanos] to provide a monotonic wall-clock time.
  *
- * This avoids the downside of [System.currentTimeMillis] not being monotonic on Android (i.e.
- * the clock doesn't consistently tick when the process is in the background or cached). It also
- * avoids the problem of [SystemClock.elapsedRealtimeNanos] not providing wall-clock time.
+ * This avoids the downside of [System.currentTimeMillis] not being monotonic
+ * (e.g. clock adjustments via NTP or manual changes causing time to jump or move backwards).
+ * It also avoids the problem of [SystemClock.elapsedRealtimeNanos] not providing wall-clock time.
  */
 public actual fun getCurrentTimeNanos(): Long = AndroidClock.now()
 
