9c6c8435313fe3bf1efc02b317c3fa411eef8bef
Viorel Alexandrescu <01-corded.syllabi@icloud.com>
2026-09-25 13:16:12 +0300

    added maven publish plugin to serializable-models module, moved test-fakes module from commonMain sourceSet to commonTest

--- serializable-models/build.gradle.kts
+++ serializable-models/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     kotlin("multiplatform")
     id("com.android.kotlin.multiplatform.library")
     id("io.opentelemetry.kotlin.build-logic")
+    id("com.vanniktech.maven.publish")
     alias(libs.plugins.kotlin.serialization)
 }
 
@@ -9,9 +10,11 @@ kotlin {
     sourceSets {
         commonMain.dependencies {
             implementation(project(":api-ext"))
-            implementation(project(":test-fakes"))
             implementation(project(":semconv"))
             implementation(libs.kotlin.serialization)
+        }
+        commonTest.dependencies {
+            implementation(project(":test-fakes"))
         }
     }
 }
